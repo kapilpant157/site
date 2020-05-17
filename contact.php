3e378c1ab273ea8557593bc805c0c0872f03f128
Kapil Dev Pant <40812026+kapilpant157@users.noreply.github.com>
2020-05-17 16:02:38 +0545

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$heading = $_POST['subject'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "kapilpant157@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
