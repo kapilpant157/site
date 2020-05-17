b4b6a5bf4ad8940d9172ab5cc365a01eb52fabb8
Kapil Dev Pant <40812026+kapilpant157@users.noreply.github.com>
2020-05-17 23:08:01 +0545

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,20 +1,25 @@
 <?php
-if(isset($_POST['submit'])){
+
 $name = $_POST['name'];
 $mailFrom = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+
+
 $email_from = 'Website';
+
 $email_body = "Name: $name.\n".
-"Email: $$email".
+"Email: $email".
 "Subject: $subject".
 "Message: $message";
-$txt = "You have a message from ".$name;".\n\n".$message;
-$mailTo = "kapilpant157@gmail.com";
+
+
+$to = "kapilpant157@gmail.com";
 $headers ="From: ".$mailFrom;
-mail($mailTo, $subject, $txt, $headers);
+$headers ="Reply=To: $mailFrom";
+
+mail($too, $subject, $txt, $headers);
 
 header("Location: index.htmlmailsent");
-}
 
 ?>
