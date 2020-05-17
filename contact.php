9584d898ee91f2b70d341168fc5b51556c26a007
Kapil Dev Pant <40812026+kapilpant157@users.noreply.github.com>
2020-05-17 22:35:36 +0545

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])){
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailTo = "kapilpant157@gmail.com";
+$headers ="From: ".$mailFrom;
+$txt = "You have a message from ".$name;".\n\n".$message;
+ 
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.htmlmailsent");
+} 
